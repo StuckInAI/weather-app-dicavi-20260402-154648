aed51fa22fa5558a054cb17216de624fc8127b3d
kfarooq1981 <148969272+kfarooq1981@users.noreply.github.com>
2026-04-02 20:48:27 +0500

    Initial commit from Flare Codegen (API)

---
+++ types/weather.ts
@@ -0,0 +1,15 @@
+export interface WeatherData {
+  city: string;
+  country: string;
+  temperature: number;
+  feelsLike: number;
+  humidity: number;
+  windSpeed: number;
+  description: string;
+  icon: string;
+  condition: string;
+  visibility: number;
+  pressure: number;
+  sunrise: number;
+  sunset: number;
+}
